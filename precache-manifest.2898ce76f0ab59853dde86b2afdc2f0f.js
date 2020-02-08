bb39f215f46e44851fe263ecaa0c3ef18adf275a
TUanphan nam <phantuanw.o.t@gmail.com>
2020-02-08 20:41:29 +0700

    add

---
+++ precache-manifest.2898ce76f0ab59853dde86b2afdc2f0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea41e95b8dcdef16e751",
+    "url": "/static/css/main.8ab80e09.chunk.css"
+  },
+  {
+    "revision": "ea41e95b8dcdef16e751",
+    "url": "/static/js/main.d16627b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "65dff4bb236bf37ff1a8",
+    "url": "/static/js/2.5ba26064.chunk.js"
+  },
+  {
+    "revision": "7a77301f5a15e5a0b5f280b4a7e388bb",
+    "url": "/index.html"
+  }
+];
